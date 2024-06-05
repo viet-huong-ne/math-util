13141d01ed57b068291fe6bf54dd97a1595aabdb
viet-huong-ne <viethuong179@gmail.com>
2024-06-05 17:17:30 +0700

    first upload

---
+++ src/main/java/com/pinky/mathutil/main/Main.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pinky.mathutil.main;
+
+import com.pinky.mathutil.core.MathUtility;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //CÁCH KIỂM THỬ 2 - GIỐNG CÁCH 1
+        //Test case #1: check if 0! returns 1
+        int n = 0;
+        long expectedResult = 1; //0! == 1
+        long actualResult = MathUtility.getFactorial(n);
+        String msg = "0!= | Expected: " + expectedResult + " Actual: " + actualResult;
+        JOptionPane.showMessageDialog(null, msg);
+    }
+
+//
+//    public static void main(String[] args) {
+//        // ĐÂY LÀ NƠI TEST HÀM
+//        // VIẾT RA CÁC TEST CASE: BỘ DATA ĐẦU VÀO VÀ GIÁ TRỊ KÌ VỌNG
+//        // LÁT HỒI SO SÁNH VỚI GIÁ TRỊ THỰC TẾ MÀ HÀM TRẢ VỀ!!!
+//        // Test case #1: n = 0 | 0! = hy vọng == 1 | thực tế chờ hàm trả về
+//        int n = 0;
+//        long expectedValue = 1;
+//        long actualValue;
+//        actualValue = MathUtility.getFactorial(n);
+//        //so sánh giữa expect vs actual coi có == nhau hay ko
+//        System.out.println("0! | Expected:" + expectedValue + " Actual: " + actualValue);
+//        
+//        //TEST CASE #2: 1! TA HY VỌNG HÀM TRẢ 1 LUÔN!!
+//        n = 1;
+//        expectedValue = 1;
+//        actualValue = MathUtility.getFactorial(n);
+//        System.out.println("1! | Expected:" + expectedValue + " Actual: " + actualValue);
+//        
+//        //TEST CASE #3: 5! TA HY VỌNG HÀM TRẢ 120 LUÔN!!
+//        n = 5;
+//        expectedValue = 120;
+//        actualValue = MathUtility.getFactorial(n);
+//        System.out.println("5! | Expected:" + expectedValue + " Actual: " + actualValue);
+//        
+//        //TEST CASE #4: -1! TA HY VỌNG HÀM NÉM RA NGOẠI LỆ!!
+//        //DO -1 KO TÍNH ĐC GIAI THỪA, ĐƯA DATA CÀ CHỚN, HÀM SẼ NÉM RA NGOẠI LỆ
+//        n = -1;
+//        //expectedValue = HÀM TUNG RA NGOẠI LỆ;
+//        System.out.println("Check getF(-1) to see the exception!");
+//        actualValue = MathUtility.getFactorial(n);
+//        //HY VỌNG SẼ THẤY NGOẠI LỆ XUẤT HIỆN
+//
+//    }
+//
+
+}
---
+++ src/test/java/com/pinky/mathutil/core/test/MathUtilityTest.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.pinky.mathutil.core.test;
+
+import com.pinky.mathutil.core.MathUtility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+//quy ước đặt tên package của Java
+//tên miền đảo ngược, chữ thường 100%
+//com.mycompany trên mạng
+//com.tên-cty.tên-dự-án.tên-module.Tên-Class-Sẽ-Dùng
+//com.microsoft.sqlserver.jdbc.
+//org.junit.jupiter.api.Test
+//com.viethuong.mathutil.core
+
+//C# viết chiều xuôi, 
+/**
+ *
+ * @author Admin
+ */
+public class MathUtilityTest {
+
+    public MathUtilityTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    //ở đây chứa các lệnh các hàm dùng để đi test hàm chỉnh ở bên code 
+    //ở tên thư mục source phía trên coi hàm chạy ổn ko
+    // ở đây ta làm các hàm ứng với các test case để test code chính ở bên 
+    //MathUtility. ví dụ .getFactorical()
+    //TEST CASE: BỘ DATA ĐƯA VÀO HÀM, APP ĐỂ VERIFY HÀM, APP CÓ XỬ LÍ
+    //ĐÚNG HAY KO, NÓ ĐI KÈM THEO CÁCH XÀI HÀM, TRÌNH TỰ XÀI APP: EXPECTED
+    //VALUE : STATUS TEST CASE PASSED HAY FAILED
+    //TEST CASE #1: CHECK GETFACTORIAL
+    //TÊN HÀM mang ý nghĩa của test case luôn 
+    @Test
+    public void testFactorialGivenRightArg0ReturnWell() {
+        int n = 0;
+        long expectedValue = 1;
+        long actualValue;
+        // nếu expected == actual, hàm ngon với case này
+
+        actualValue = MathUtility.getFactorial(n);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    //TEST CASE #2; CHECK GETFACTORIAL WITH N = 1, EXPECTED => 1! = 1
+    @Test
+    public void testFactorialGivenRightArglReturnsWell() {
+        int n = 1;
+        long expectedValue = 1;
+        long actualValue;
+        actualValue = MathUtility.getFactorial(n);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFactorialGivenRighArg235ReturnsWell() {
+        int n = 2;
+        long expectedValue = 2;
+        long actualValue;
+        actualValue = MathUtility.getFactorial(n);
+        assertEquals(expectedValue, actualValue);
+        
+        assertEquals(6, MathUtility.getFactorial(3));
+
+        assertEquals(120, MathUtility.getFactorial(5));
+    }
+}
